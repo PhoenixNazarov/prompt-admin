f99060dddf71f3e05e9708e21a87238337c266b9
phoenix <vova24848@gmail.com>
2024-09-09 09:45:16 +0300

    update to 0.1.18 bugfix

--- library/promptadmin/types/settings.py
+++ library/promptadmin/types/settings.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 class PromptAdminSettings(BaseModel):
     router_secret: str = os.getenv('PROMPT_ADMIN_ROUTER_SECRET')
     var_connection: str = os.getenv('PROMPT_ADMIN_VAR_CONNECTION')
+    test_rerun_timeout: int = 60 * 60 * 6
 
 
 class Settings(BaseModel):
