77322439e569726ac62c52762622e05273acde40
phoenix <vova24848@gmail.com>
2024-09-21 12:26:52 +0300

    update to 0.1.21. add support for anthropic_bedrock

--- library/settings.py
+++ library/settings.py
@@ -4,6 +4,9 @@ from promptadmin.types import Settings as PromptAdminSettings
 class Settings(PromptAdminSettings):
     app: str = 'prompt-admin'
     anthropic_key: str = ''
+    aws_secret_key: str = ''
+    aws_access_key: str = ''
+    aws_region: str = ''
 
 
 SETTINGS = Settings()
